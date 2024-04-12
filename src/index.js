9e18193f938161870944c914231db5629fd70b46
Phan thanh Lộc <phanthanhloc@192.168.2.10>
2024-04-12 23:04:40 +0700

    [config] init project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const morgan = require('morgan')
+app.use(morgan('combined'))
+app.get('/', (req, res) => {
+  res.send('Hello World!s')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
