3b2e689969ca3a1cc0f50b942af42a98632c2153
Phan thanh Lộc <phanthanhloc@192.168.2.10>
2024-04-21 00:56:02 +0700

    [feat] Add address delete  feature

--- src/controllers/AddressControllers.js
+++ src/controllers/AddressControllers.js
@@ -58,6 +58,7 @@ class AddressController {
       });
     }
   }
+  //update address
   async updateAdderss(req, res) {
     const idUser = req.userId;
     const idAddress = req.params.id;
@@ -121,7 +122,6 @@ class AddressController {
           id: idAddress,
         },
       });
-
       return res.status(200).json({
         status: "succsess",
         data: data,
@@ -129,6 +129,44 @@ class AddressController {
     } catch (error) {
       return res.status(500).json({
         status: "error",
+        message: error.message,
+      });
+    }
+  }
+  // delete address
+  async deleteAddress(req, res) {
+    const idAddress = req.params.id;
+    try {
+      const ischeck = await Address.findOne({
+        where: {
+          id: idAddress,
+        },
+      });
+      if (!ischeck) {
+        return res.status(400).json({
+          status: "error",
+          message: "address not found",
+        });
+      }
+
+      if (ischeck.is_default === true) {
+        return res.status(400).json({
+          status: "error",
+          message: "Default address cannot be deleted",
+        });
+      }
+      const data = await Address.destroy({
+        where: {
+          id: idAddress,
+        },
+      });
+      return res.status(200).json({
+        status: "succsess",
+        data: ischeck,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: "error ",
         message: error.message,
       });
     }
--- src/routers/address.js
+++ src/routers/address.js
@@ -4,4 +4,5 @@ const authMiddleware = require("../middleware/authMiddleware")
 const router = express.Router();
 router.post("/create-address" ,authMiddleware , addressController.createAddress);
 router.put("/update-address/:id" ,authMiddleware , addressController.updateAdderss);
+router.delete("/delete-address/:id" ,authMiddleware , addressController.deleteAddress);
 module.exports = router;
