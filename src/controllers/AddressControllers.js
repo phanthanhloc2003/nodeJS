d461d159807abe9945ee44a22a80863728262531
Phan thanh Lộc <phanthanhloc@192.168.2.10>
2024-04-21 00:31:12 +0700

    [feat] Add address update feature

--- src/controllers/AddressControllers.js
+++ src/controllers/AddressControllers.js
@@ -58,5 +58,80 @@ class AddressController {
       });
     }
   }
+  async updateAdderss(req, res) {
+    const idUser = req.userId;
+    const idAddress = req.params.id;
+    const {
+      region,
+      region_id,
+      district,
+      district_id,
+      ward,
+      ward_id,
+      full_Name,
+      telephone,
+      street,
+      is_default,
+    } = req.body;
+    const phoneRegex = /^\d{10,12}$/;
+    if (!phoneRegex.test(telephone)) {
+      return res.status(400).json({
+        status: "error",
+        message: "invalid phone number",
+      });
+    }
+    if (!idAddress) {
+      return res.status(400).json({
+        status: "error",
+        message: "not found id",
+      });
+    }
+    try {
+      if (is_default === 1) {
+        await Address.update(
+          { is_default: 0 },
+          {
+            where: {
+              user_id: idUser,
+            },
+          }
+        );
+      }
+      await Address.update(
+        {
+          region: region,
+          region_id: region_id,
+          district: district,
+          district_id: district_id,
+          ward: ward,
+          ward_id: ward_id,
+          full_Name: full_Name,
+          telephone: telephone,
+          street: street,
+          is_default: is_default,
+        },
+        {
+          where: {
+            id: idAddress,
+          },
+        }
+      );
+      const data = await Address.findOne({
+        where: {
+          id: idAddress,
+        },
+      });
+
+      return res.status(200).json({
+        status: "succsess",
+        data: data,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: "error",
+        message: error.message,
+      });
+    }
+  }
 }
 module.exports = new AddressController();
--- src/routers/address.js
+++ src/routers/address.js
@@ -3,4 +3,5 @@ const addressController = require('../controllers/AddressControllers')
 const authMiddleware = require("../middleware/authMiddleware")
 const router = express.Router();
 router.post("/create-address" ,authMiddleware , addressController.createAddress);
+router.put("/update-address/:id" ,authMiddleware , addressController.updateAdderss);
 module.exports = router;
